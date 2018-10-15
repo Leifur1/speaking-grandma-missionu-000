9522cd7da9cb4c4e275594b5d667dce1d8d39e08
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-15 23:04:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(response)
-  if response.match(/[A-Z]/) == true
+  if response.match(/[A-Z]+/) == true
     puts "NO, NOT SINCE 1938!"
   elsif response == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
